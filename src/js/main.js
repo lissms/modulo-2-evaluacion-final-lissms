3e97d1c5281e6842fe66cf61ba840c5b62572cfa
lissms <felissms@gmail.com>
2020-10-22 19:27:25 +0200

    add series search

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,16 @@ const button = document.querySelector(".js-search-button");
 let resultsSearch = document.querySelector(".js-search-input");
 /* obtengo el valor del input */
 const addInputValue = () => {
-  console.log("object", resultsSearch.value);
-
   fetch(`http://api.tvmaze.com/search/shows?q=${resultsSearch.value}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log("input value", resultsSearch.value);
-      console.log("api name", data[1].show.name);
-      console.log("api img", data[1].show.image.medium);
-      paintCard(data[1].show.image.medium, data[1].show.name);
+      for (let i = 0; i < data.length; i++) {
+        paintCard(
+          data[i].show.image.medium,
+          data[i].show.name
+        ); /* llamo a la funcion q pinta */
+      }
     });
-
-  /* debo meter funcion de pintar */
 };
 
 addInputValue();
@@ -24,14 +22,10 @@ button.addEventListener("click", addInputValue);
 /* paints funstions */
 const paintCard = (src, name) => {
   const containerCardSeries = document.querySelector(".js-container-result");
-  const array = ["prueba", "prueba"];
-
-  for (let i = 0; i < array.length; i++) {
-    containerCardSeries.innerHTML += ` <div class="js-info-of-my-serie info-of-my-serie">
+  containerCardSeries.innerHTML += ` <div class="js-info-of-my-serie info-of-my-serie">
       <li class="js-results">
-        <img src="${src}" alt="" />
+        <img src="${src}" alt="image of series" />
       </li>
       <h3 class="js-name-series">${name}</h3>
       </div>`;
-  }
 };
