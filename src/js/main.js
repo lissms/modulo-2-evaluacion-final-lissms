9d963dc0f0422e0b196e3f95aac3dfca82a4a854
lissms <felissms@gmail.com>
2020-10-23 11:48:24 +0200

    add funstions paint

--- src/js/main.js
+++ src/js/main.js
@@ -2,28 +2,76 @@
 /* me llamo al boton */
 const button = document.querySelector(".js-search-button");
 let resultsSearch = document.querySelector(".js-search-input");
+let showsList;
 /* obtengo el valor del input */
 const addInputValue = () => {
   fetch(`http://api.tvmaze.com/search/shows?q=${resultsSearch.value}`)
     .then((response) => response.json())
     .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        paintCard(
-          data[i].show.image.medium,
-          data[i].show.name
-        ); /* llamo a la funcion q pinta */
-      }
+      showsList = data;
+      paintAllCards();
     });
 };
 
 addInputValue();
 button.addEventListener("click", addInputValue);
 
-/* paints funstions */
-const paintCard = (src, name) => {
-  const containerCardSeries = document.querySelector(".js-container-result");
-  containerCardSeries.innerHTML += ` <div class="js-info-of-my-serie info-of-my-serie">
-        <img src="${src}" alt="image of series" class = "image-card" />
-      <h3 class="js-name-series">${name}</h3>
-      </div>`;
+const paintAllCards = () => {
+  containerCardsSeries.innerHTML = "";
+
+  for (let i = 0; i < showsList.length; i++) {
+    paintCard(
+      showsList[i].show.image.medium,
+      showsList[i].show.name,
+      i
+    ); /* llamo a la funcion q pinta */
+  }
+  assignListener();
 };
+
+/* paints funstions */
+const containerCardsSeries = document.querySelector(".js-list-of-series");
+const paintCard = (src, name, id) => {
+  /*  containerCardsSeries.innerHTML = "";  */
+
+  containerCardsSeries.innerHTML += ` 
+  <li id = ${id} class="js-results">
+  <div class="js-info-of-my-serie info-of-my-serie">
+  <img class="image card" src="${src}" alt=""
+    <h3 class="js-name-series"> ${name}</h3>
+  </div>
+</li>`;
+  /////////////////////////////
+};
+
+/* paint favorite */
+/* const favoriteCard = document.querySelector(".js-results");
+
+const paintFavoriteCard = (ev) => {
+  console.log("favorita", ev.currentTarget.id);
+};
+
+favoriteCard.addEventListener("click", paintFavoriteCard); */
+
+////////////////////////////////////
+
+const assignListener = () => {
+  let containerCards = document.querySelectorAll(".js-results");
+  for (let i = 0; i < containerCards.length; i++) {
+    containerCards[i].addEventListener("click", addFavoriteSeries);
+  }
+};
+
+const addFavoriteSeries = (even) => {
+  console.log("object", even.currentTarget.id);
+};
+
+///////////////////////////////////////// /* con esta funcion algunos identifica el id y otros no */
+/* const addFavoriteSeries = (even) => {
+  const hello = even.target.id;
+  console.log("object", hello);
+};
+
+containerCardsSeries.addEventListener("click", addFavoriteSeries); */
+
+////////////////////////////
